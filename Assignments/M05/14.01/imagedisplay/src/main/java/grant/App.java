6e9eccf4fcf7d5a9954cee836da32afe750d560c
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2024-07-02 07:51:52 -0400

    removed unused code

--- Assignments/M05/14.01/imagedisplay/src/main/java/grant/App.java
+++ Assignments/M05/14.01/imagedisplay/src/main/java/grant/App.java
@@ -2,10 +2,8 @@ package grant;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -56,15 +54,6 @@ public class App extends Application {
         stage.setTitle("Flags");
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
