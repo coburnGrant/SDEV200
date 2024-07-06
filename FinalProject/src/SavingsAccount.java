f50a29091ff79e24b7ab40e15dc2a4f891dce1f6
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2024-07-06 10:09:57 -0400

    added getter and setter for interest rate

--- FinalProject/src/SavingsAccount.java
+++ FinalProject/src/SavingsAccount.java
@@ -2,12 +2,22 @@ package FinalProject.src;
 
 public class SavingsAccount extends Account {
     /** Interest rate this savings account has */
-    double interestRate;
+    private double interestRate;
 
     /** Constructor for a savings account */
     public SavingsAccount(String userID, String name, double interestRate) {
         super(userID, name, AccountType.SAVINGS);
         
+        this.interestRate = interestRate;
+    }
+
+    /** Getter for interest rate */
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    /** Setter for interest rate */
+    public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
 }
