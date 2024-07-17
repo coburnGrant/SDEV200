3e2aa207eaffb23740b7eb43e71e7f0106ce0aea
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2024-07-17 08:03:53 -0400

    cleaned up code

--- Assignments/M07/35.05/loan-calculator/src/main/java/com/grant/LoanCalculator.java
+++ Assignments/M07/35.05/loan-calculator/src/main/java/com/grant/LoanCalculator.java
@@ -12,31 +12,26 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LoanCalculator extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    public LoanCalculator() {
-        super();
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Get form info
         double loanAmount = Double.parseDouble(request.getParameter("loanAmount"));
-        double annualInterestRate =
-        Double.parseDouble(request.getParameter("annualInterestRate"));
+        double annualInterestRate = Double.parseDouble(request.getParameter("annualInterestRate"));
         int numberOfYears = Integer.parseInt(request.getParameter("numberOfYears"));
 
+        // Make loan
         Loan loan = new Loan(annualInterestRate, numberOfYears, loanAmount);
         double monthlyPayment = loan.getMonthlyPayment();
         double totalPayment = loan.getTotalPayment();
 
+        // Print results
         response.setContentType("text/html");
         response.getWriter().println("<html><body>");
         response.getWriter().println("<h2>Loan Details</h2>");
         response.getWriter().println("<p>Loan Amount: " + loanAmount + "</p>");
-        response.getWriter().println("<p>Annual Interest Rate: " + annualInterestRate
-        + "</p>");
-        response.getWriter().println("<p>Number of Years: " + numberOfYears +
-        "</p>");
-        response.getWriter().println("<p>Monthly Payment: " + monthlyPayment +
-        "</p>");
+        response.getWriter().println("<p>Annual Interest Rate: " + annualInterestRate + "</p>");
+        response.getWriter().println("<p>Number of Years: " + numberOfYears + "</p>");
+        response.getWriter().println("<p>Monthly Payment: " + monthlyPayment + "</p>");
         response.getWriter().println("<p>Total Payment: " + totalPayment + "</p>");
         response.getWriter().println("</body></html>");
     }
